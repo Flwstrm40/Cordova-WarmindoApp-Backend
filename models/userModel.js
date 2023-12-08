5be977f5f76d120221565a7f9f9a13a5235dd54f
Muhammad Naufal Arkan <naufalarkan@students.undip.ac.id>
2023-12-08 23:21:49 +0700

    add login backend

---
+++ models/userModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+    {
+        id_pengguna: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: false,
+            default: 'warmindo'
+        },
+        nama_pengguna: {
+            type: String,
+            required: true
+        },
+        id_role: {
+            type: String,
+            enum: ['E1', 'E2', 'E3', 'E4'],
+            required: true
+        },
+        status: {
+            type: String,
+            enum: ['aktif', 'tidak aktif'],
+            default: 'aktif'
+        },
+        foto: {
+            type: String,  // URL gambar atau path file
+            required: false
+        }
+    }
+);
+
+
+// Middleware untuk mengenkripsi password sebelum menyimpan user
+userSchema.pre('save', async function (next) {
+    try {
+        const hashedPassword = await bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,14 +1,81 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/userModel');
+const bcrypt = require('bcrypt');
+const cors = require('cors'); 
 const app = express();
-// const mongoose = require('mongoose');
 
+
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:8000',  // Ganti dengan origin yang sesuai
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}));
 
 // routes
+
+
+// Endpoint untuk login
+app.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+
+        // Jika login berhasil, kirim data user yang berhasil login
+        res.status(200).json({ user });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+});
 
 app.get('/', (req, res) => {
     res.send('Selamat datang di proyek Express.js dengan MongoDB!');
 })
+app.get('/blog', (req, res) => {
+    res.send('Blog!!! halo ini blog');
+})
 
-app.listen(3000, () => {
-    console.log('Server berjalan di http://localhost:3000, silahkan buka di browser!!!')
-});
+app.get('/user', async(req, res) => {
+    try{
+        const user = await User.find({});
+        res.status(200).json({user});
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
+
+app.post('/user', async(req, res) =>{
+    try{
+        const user = await User.create(req.body);
+        res.status(200).json({user});
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send({message: error.message});
+    }
+})
+
+
+mongoose.set("strictQuery", false);
+mongoose
+.connect('mongodb+srv://flwstrm:Vanitas_40@cluster0.he7vqbb.mongodb.net/node-API?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000, () => {
+        console.log('Server berjalan di http://localhost:3000')
+    });
+    console.log('Connected to mongoDB!');
+}).catch(() => {
+    console.log(error);
+})
